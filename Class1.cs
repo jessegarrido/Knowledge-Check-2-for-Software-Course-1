ae25d5a5bfd9a1b4e85108a7f1b6736d2f80894d
relooreloo <garrido.jesse@gmail.com>
2024-09-15 11:37:56 -0400

    I did it!

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace KnowledgeCheck2
+{
+
+    public class Instrument
+    { 
+        public string ManufacturerName { get; set; } = string.Empty;
+        public string ModelName { get; set; } = string.Empty;
+    }
+    public class Guitar : Instrument
+    {
+        public int NumberOfStrings { get; set; } = 6;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace KnowledgeCheck2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  Console.WriteLine("Hello, World!");
+            Console.WriteLine("How many records do you want to add? ");
+            var numberOfRecords = int.Parse(Console.ReadLine());
+            var recordList = new List<Guitar>();
+            for (int i = 0; i < numberOfRecords; i++)
+            {
+                // In this loop, populate the object's properties using Console.ReadLine()
+                var myClass = new Guitar();
+                Console.Write("Enter a Manufacturer Name: ");
+                myClass.ManufacturerName = Console.ReadLine();
+                Console.Write("Enter a Model Name: ");
+                myClass.ModelName = Console.ReadLine();
+                Console.Write("Enter a Number of Strings: ");
+                myClass.NumberOfStrings = int.Parse(Console.ReadLine());
+                recordList.Add(myClass);
+            }
+            // Print out the list of records using Console.WriteLine()    
+            foreach (Guitar guitar in recordList)
+            {
+                Console.WriteLine($"Manufacturer Name: {guitar.ManufacturerName}");
+                Console.WriteLine($"Model Name: {guitar.ModelName}");
+                Console.WriteLine($"Number of Strings: {guitar.NumberOfStrings}");
+            }
+        }
+    }
+}
